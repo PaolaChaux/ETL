1e28e2c06a0a6312ca4c17a477ed9973f5e7b876
StevenVegaL <stivenlopez143@gmail.com>
2024-05-03 13:08:10 -0500

    Steven

--- Airflow_water/transform_dag.py
+++ Airflow_water/transform_dag.py
@@ -147,54 +147,54 @@ def apply_transformations(water):
 
 # tranformaciones API:
 
-# def convert_dates(df):
-#     df['fecha_terminacion_proyecto'] = pd.to_datetime(df['fecha_terminacion_proyecto'])
-#     df['fecha_de_corte'] = pd.to_datetime(df['fecha_de_corte'])
-#     return df
+def convert_dates(df):
+    df['fecha_terminacion_proyecto'] = pd.to_datetime(df['fecha_terminacion_proyecto'])
+    df['fecha_de_corte'] = pd.to_datetime(df['fecha_de_corte'])
+    return df
 
-# def normalize_text_columns(df):
-#     str_cols = df.select_dtypes(include=['object']).columns
-#     df[str_cols] = df[str_cols].apply(lambda x: x.str.lower().str.strip())
-#     return df
+def normalize_text_columns(df):
+    str_cols = df.select_dtypes(include=['object']).columns
+    df[str_cols] = df[str_cols].apply(lambda x: x.str.lower().str.strip())
+    return df
 
-# def compute_num_municipios(df):
-#     df['num_municipios'] = df['c_digo_divipola_municipio'].apply(lambda x: len(x.split(',')))
-#     return df
+def compute_num_municipios(df):
+    df['num_municipios'] = df['c_digo_divipola_municipio'].apply(lambda x: len(x.split(',')))
+    return df
 
-# def map_regions(df):
-#     region_mapping = {
-#         'CHOCO': 'Región Pacífica', 'CAUCA': 'Región Pacífica', 'NARIÑO': 'Región Pacífica', 'VALLE DEL CAUCA': 'Región Pacífica',
-#         'ARAUCA': 'Región Orinoquía', 'CASANARE': 'Región Orinoquía', 'GUAINÍA': 'Región Orinoquía', 'GUAVIARE': 'Región Orinoquía', 'META': 'Región Orinoquía', 'VICHADA': 'Región Orinoquía',
-#         'AMAZONAS': 'Región Amazonía', 'CAQUETA': 'Región Amazonía', 'PUTUMAYO': 'Región Amazonía', 'VAUPES': 'Región Amazonía',
-#         'ANTIOQUIA': 'Región Andina', 'BOYACA': 'Región Andina', 'CALDAS': 'Región Andina', 'CUNDINAMARCA': 'Región Andina', 'HUILA': 'Región Andina', 'NORTE DE SANTANDER': 'Región Andina', 'QUINDIO': 'Región Andina', 'RISARALDA': 'Región Andina', 'SANTANDER': 'Región Andina', 'TOLIMA': 'Región Andina',
-#         'SAN ANDRES Y PROVIDENCIA': 'Región Insular', 'ATLANTICO': 'Región Caribe', 'BOLIVAR': 'Región Caribe', 'CESAR': 'Región Caribe', 'CORDOBA': 'Región Caribe', 'LA GUAJIRA': 'Región Caribe', 'MAGDALENA': 'Región Caribe', 'SUCRE': 'Región Caribe'
-#     }
-#     df['departamento'] = df['departamento'].replace({
-#         'SAN ANDRES': 'SAN ANDRES Y PROVIDENCIA',
-#         'N DE SANTANDER': 'NORTE DE SANTANDER'
-#     }).str.upper()
-#     df['región'] = df['departamento'].map(region_mapping).fillna('Región Desconocida')
-#     return df
+def map_regions(df):
+    region_mapping = {
+        'CHOCO': 'Región Pacífica', 'CAUCA': 'Región Pacífica', 'NARIÑO': 'Región Pacífica', 'VALLE DEL CAUCA': 'Región Pacífica',
+        'ARAUCA': 'Región Orinoquía', 'CASANARE': 'Región Orinoquía', 'GUAINÍA': 'Región Orinoquía', 'GUAVIARE': 'Región Orinoquía', 'META': 'Región Orinoquía', 'VICHADA': 'Región Orinoquía',
+        'AMAZONAS': 'Región Amazonía', 'CAQUETA': 'Región Amazonía', 'PUTUMAYO': 'Región Amazonía', 'VAUPES': 'Región Amazonía',
+        'ANTIOQUIA': 'Región Andina', 'BOYACA': 'Región Andina', 'CALDAS': 'Región Andina', 'CUNDINAMARCA': 'Región Andina', 'HUILA': 'Región Andina', 'NORTE DE SANTANDER': 'Región Andina', 'QUINDIO': 'Región Andina', 'RISARALDA': 'Región Andina', 'SANTANDER': 'Región Andina', 'TOLIMA': 'Región Andina',
+        'SAN ANDRES Y PROVIDENCIA': 'Región Insular', 'ATLANTICO': 'Región Caribe', 'BOLIVAR': 'Región Caribe', 'CESAR': 'Región Caribe', 'CORDOBA': 'Región Caribe', 'LA GUAJIRA': 'Región Caribe', 'MAGDALENA': 'Región Caribe', 'SUCRE': 'Región Caribe'
+    }
+    df['departamento'] = df['departamento'].replace({
+        'SAN ANDRES': 'SAN ANDRES Y PROVIDENCIA',
+        'N DE SANTANDER': 'NORTE DE SANTANDER'
+    }).str.upper()
+    df['región'] = df['departamento'].map(region_mapping).fillna('Región Desconocida')
+    return df
 
-# def calculate_financing(df):
-#     df['total_financiamiento'] = df['aporte_nacion'] + df['contrapartida']
-#     return df
+def calculate_financing(df):
+    df['total_financiamiento'] = df['aporte_nacion'] + df['contrapartida']
+    return df
 
-# def calculate_project_duration(df):
-#     df['duracion_proyecto_dias'] = (df['fecha_de_corte'] - df['fecha_terminacion_proyecto']).dt.days
-#     return df
+def calculate_project_duration(df):
+    df['duracion_proyecto_dias'] = (df['fecha_de_corte'] - df['fecha_terminacion_proyecto']).dt.days
+    return df
 
-# def drop_unnecessary_columns(df):
-#     df.drop(['fecha_de_corte', 'fecha_terminacion_proyecto', 'contrapartida', 'aporte_nacion'], axis=1, inplace=True)
-#     return df
+def drop_unnecessary_columns(df):
+    df.drop(['fecha_de_corte', 'fecha_terminacion_proyecto', 'contrapartida', 'aporte_nacion'], axis=1, inplace=True)
+    return df
 
-# def transform_api_data(df):
-#     df = convert_dates(df)
-#     df = normalize_text_columns(df)
-#     df = compute_num_municipios(df)
-#     df = map_regions(df)
-#     df = calculate_financing(df)
-#     df = calculate_project_duration(df)
-#     df = drop_unnecessary_columns(df)
-#     return df
+def transformations_api_data(df):
+    df = convert_dates(df)
+    df = normalize_text_columns(df)
+    df = compute_num_municipios(df)
+    df = map_regions(df)
+    df = calculate_financing(df)
+    df = calculate_project_duration(df)
+    df = drop_unnecessary_columns(df)
+    return df
 
