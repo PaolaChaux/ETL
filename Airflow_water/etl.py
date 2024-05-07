34cc017b763a2e3883306ac67bc4975608cf86bc
Paola Chaux Campo <chauxcampo@gmail.com>
2024-05-07 11:41:23 -0500

    pao

--- Airflow_water/etl.py
+++ Airflow_water/etl.py
@@ -10,8 +10,6 @@ from transform_dag import transformations_water
 
 
 
-
-
 def read_water():
     with open('./dag_water/db_config.json') as file:
         db_config = json.load(file)
@@ -86,3 +84,83 @@ def transform_api(**kwargs):
 #     logging.info("Cargando datos...")
     
 #     logging.info("Los datos se han cargado en: tracks")
+
+
+
+def load(**kwargs):
+    ti = kwargs['ti']
+    json_data = json.loads(ti.xcom_pull(task_ids="merge_task"))
+    logging.info(f"Data coming from extract: {json_data}")
+    
+    df_water = pd.json_normalize(json_data)
+    df_water.fillna(value=pd.NA, inplace=True)  # Normalizar NaN a NA para compatibilidad con la base de datos
+
+    try:
+        with open('db_config.json', 'r') as config_json, psycopg2.connect(**json.load(config_json)) as conx:
+            cursor = conx.cursor()
+            # Creación de tablas de dimensiones y tabla de hechos
+            cursor.execute("""
+                CREATE TABLE IF NOT EXISTS dimension_date (
+                    "ID_Tiempo" SERIAL PRIMARY KEY,
+                    "Año" DATE NOT NULL
+                );
+            """)
+            cursor.execute("""
+                CREATE TABLE IF NOT EXISTS dimension_ubication (
+                    "ID_Ubicacion" SERIAL PRIMARY KEY,
+                    "nombre_departamento" VARCHAR(255) NOT NULL,
+                    "div_dpto" INT NOT NULL,
+                    "nombre_municipio" VARCHAR(255) NOT NULL,
+                    "divi_muni" INT NOT NULL
+                );
+            """)
+            cursor.execute("""
+                CREATE TABLE IF NOT EXISTS dimension_parameters (
+                    "ID_Parametro" SERIAL PRIMARY KEY,
+                    "nombre_parametro_analisis" VARCHAR(255) NOT NULL
+                );
+            """)
+            cursor.execute("""
+                CREATE TABLE IF NOT EXISTS dimension_tratamiento (
+                    "ID_Rango" SERIAL PRIMARY KEY,
+                    "rango_irca" VARCHAR(255) NOT NULL,
+                    "tratamiento_categoría" VARCHAR(255) NOT NULL
+                );
+            """)
+            cursor.execute("""
+                CREATE TABLE IF NOT EXISTS Fact_WaterQuality (
+                    "ID_Medicion" SERIAL PRIMARY KEY,
+                    "ID_Tiempo" INT NOT NULL REFERENCES dimension_date("ID_Tiempo"),
+                    "ID_Ubicacion" INT NOT NULL REFERENCES dimension_ubication("ID_Ubicacion"),
+                    "ID_Parametro" INT NOT NULL REFERENCES dimension_parameters("ID_Parametro"),
+                    "ID_Rango" INT NOT NULL REFERENCES dimension_tratamiento("ID_Rango"),
+                    "irca_minimo" FLOAT NOT NULL,
+                    "irca_maximo" FLOAT NOT NULL,
+                    "irca_promedio" FLOAT NOT NULL,
+                    "numero_parametros_promedio" INT NOT NULL,
+                    "porcentaje_muestras_tratadas" INT NOT NULL,
+                    "diferencia_muestras_tratadas_sin_tratar" INT NOT NULL,
+                    "rango_parametros_analizados" INT NOT NULL
+                );
+            """)
+
+            for year in df_water['Año'].drop_duplicates():
+                cursor.execute("""
+                    INSERT INTO dimension_date ("Año") VALUES (%s) ON CONFLICT ("Año") DO NOTHING RETURNING "ID_Tiempo";
+                """, (year,))
+                id_tiempo = cursor.fetchone()[0] if cursor.rowcount != 0 else None
+            
+            conx.commit()
+            logging.info("Data has been successfully loaded to the database.")
+
+    except Exception as e:
+        logging.error(f"An error occurred: {e}")
+        if conx:
+            conx.rollback()
+
+    finally:
+        if cursor:
+            cursor.close()
+
+    return "Data loaded successfully"
+    
